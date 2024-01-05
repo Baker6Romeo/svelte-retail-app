a1fe942d8158d7390e485c60a5f383db263c766b
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2024-01-05 06:45:19 -0800

    svelte shorthand for product subscription

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
-  import { onDestroy, onMount } from "svelte";
   import products from "../../stores/defaultProducts";
-  import type { Unsubscriber } from "svelte/store";
-
-  let defaultProducts: any[] = [];
-  let unsubscribe: Unsubscriber;
-
-  onDestroy(() => {
-    unsubscribe();
-  });
-  onMount(() => {
-    unsubscribe = products.subscribe((products) => {
-      defaultProducts = products;
-    });
-  });
 </script>
 
 <h1>Products Component</h1>
-{#each defaultProducts as product}
+{#each $products as product}
   <h2>{product.title}</h2>
 {/each}
