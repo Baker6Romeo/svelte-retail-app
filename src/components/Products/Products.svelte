789f1e4e1caedd18eda59cab40f2ad7102dda3e1
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2024-01-05 06:08:54 -0800

    setup local store

---
+++ src/components/Products/Products.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import products from "../../stores/defaultProducts";
+  import localProducts from "../../localProducts";
+
+  let defaultProducts: any[] = [];
+
+  onMount(() => {
+    products.subscribe((products) => {
+      defaultProducts = products;
+    });
+  });
+</script>
+
+<h1>Products Component</h1>
+{#each defaultProducts as product}
+  <h2>{product.title}</h2>
+{/each}
