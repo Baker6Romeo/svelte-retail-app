198ac51e8dae5008f8c34c48d6119678c70f4f44
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2024-02-16 12:41:25 -0800

    Filter featured with reactive variable

--- src/components/Products/Featured.svelte
+++ src/components/Products/Featured.svelte
@@ -4,15 +4,19 @@
   import products from "../../stores/defaultProducts";
 
   export let title = "";
+
+  $: featuredProducts = $products.filter((item) => item.featured === true);
 </script>
 
-<section class="section">
-  <h2 class="section-title">{title}</h2>
-  <div class="products-center">
-    {#each $products as product (product.id)}
-      <Product {product} />
-    {:else}
-      <Loading />
-    {/each}
-  </div>
-</section>
+{#if featuredProducts.length === 0}
+  <Loading />
+{:else}
+  <section class="section">
+    <h2 class="section-title">{title}</h2>
+    <div class="products-center">
+      {#each featuredProducts as product (product.id)}
+        <Product {product} />
+      {/each}
+    </div>
+  </section>
+{/if}
