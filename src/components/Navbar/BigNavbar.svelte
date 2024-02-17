375654015a78550948990523dedaa61ff389d3d1
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2024-02-16 16:34:28 -0800

    Create links constant, setup big navbar

--- src/components/Navbar/BigNavbar.svelte
+++ src/components/Navbar/BigNavbar.svelte
@@ -1,12 +1,17 @@
 <script>
   import CartButton from "../Cart/CartButton.svelte";
   import { link } from "svelte-routing";
+  import links from "../../constants/links";
 </script>
 
 <nav class="navbar">
   <div class="nav-container">
     <div class="nav-center">
-      <p>nav links</p>
+      <ul class="nav-links">
+        {#each links as navLink}
+          <li><a href={navLink.url} use:link>{navLink.text}</a></li>
+        {/each}
+      </ul>
       <a href="/" use:link class="nav-logo big-logo">
         <img src="/assets/images/logo.svg" class="logo" alt="razors logo" />
       </a>
